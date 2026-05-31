51de048d46217ce26adc15b08692b97f0cecb539
jorik2018 <antoniopinedo2018@gmail.com>
2026-05-30 19:15:29 -0500

    fix working directory for poetry in openshift

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 poetry run reflex run
+
+
+oc set env deployment/reflex-erp-git \
+  API_URL=https://reflex-erp-git-epinedo-dev.apps.rm3.7wse.p1.openshiftapps.com \
+  DEPLOY_URL=https://reflex-erp-git-epinedo-dev.apps.rm3.7wse.p1.openshiftapps.com
--- rxconfig.py
+++ rxconfig.py
@@ -1,8 +1,13 @@
+import os
 import reflex as rx
 
 config = rx.Config(
     app_name="reflex_erp",
     show_built_with_reflex=False,
+
+    api_url=os.getenv("API_URL", "http://localhost:3000"),
+    deploy_url=os.getenv("DEPLOY_URL", "http://localhost:3000"),
+
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
